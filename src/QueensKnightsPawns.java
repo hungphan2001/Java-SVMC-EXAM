b4ad049b2186b0c58922410e9dd9d9d3b6077ce0
Hưng Phan <phandinhhungvp2001@gmail.com>
2024-01-30 21:12:18 +0700

    fix QueensKnightsPawns
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/QueensKnightsPawns.java
+++ src/QueensKnightsPawns.java
@@ -1,119 +1,101 @@
 import java.io.FileInputStream;
 import java.util.*;
 public class QueensKnightsPawns {
-        static int[][] board; // Lưu bàn cờ
-        static int row, col; // Số hàng, cột
-        static final int Q = 0, K = 1, P = 2, Safe = 3, Unsafe = 4;
+    public static final int NOT_SAFE = 0;
+    public static final int SAFE = 1;
+    public static final int QUEEN = 2;
+    public static final int KNIGHT = 3;
+    public static final int PAWN = 4;
 
-        // Đường đi con mã
-        static int[] K_row = {-1, -2, 1, 2, -2, -1, 2, 1};
-        static int[] K_col = {2, 1, 2, 1, -1, -2, -1, -2};
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("C:\\Users\\hung\\IdeaProjects\\learn SRV\\src\\input_queensKnightsPawns.txt"));
+        Scanner scanner = new Scanner(System.in);
+        int m = 1, n = 1, round = 1, count_safe, count_chess, count_pos, x, y;
+        int i, j, k;
+        int[][] array;
+        int[] knight_moveX = {+2, +1, -1, -2, -2, -1, +1, +2};
+        int[] knight_moveY = {+1, +2, +2, +1, -1, -2, -2, -1};
+        int[] Q_cross_moveX = {+1, +1, -1, -1};
+        int[] Q_cross_moveY = {+1, -1, +1, -1};
+        int[] Q_straight_move = {+1, -1};
 
-        /*
-         * Tính số ô con mã có thể ăn
-         *
-         * @PARAM: row, hàng của con mã đang xét
-         *
-         * @PARAM: col, cột của con mã đang xét
-         *
-         * RETURN: số vị trí con mã có thể ăn
-         */
-        static int checkK(int row, int col) {
-            int count = 0;
-            // Kiểm tra 8 hướng của con mã
-            for (int i = 0; i < 8; i++) {
-                int r = row + K_row[i];
-                int c = col + K_col[i];
-                if (r <= QueensKnightsPawns.row && r >= 0 && c <= QueensKnightsPawns.col && c >= 0) {
-                    // Nếu ô nào đang ở vị trí an toàn
-                    // thì tức là con mã có thể ăn
-                    // cho ô đó thành Unsafe để tránh lặp và tăng biến đếm
-                    if (board[r][c] == Safe) {
-                        board[r][c] = Unsafe;
-                        count++;
-                    }
+        while(true) {
+            m = scanner.nextInt();
+            n = scanner.nextInt();
+            if(m == 0 && n == 0)
+                break;
+            count_chess = QUEEN;
+            count_safe = 0;
+            array = new int[m][n];
+
+            for(i=0; i<m; i++) {
+                for(j=0; j<n; j++) {
+                    array[i][j] = SAFE;
                 }
             }
-            return count;
-        }
 
-        // Đường đi con hậu
-        static int[] Q_row = {0, 0, 1, 1, 1, -1, -1, -1};
-        static int[] Q_col = {1, -1, -1, 0, 1, -1, 0, 1};
-        static int sum = 0;
-
-        /*
-         * Duyệt mỗi con hậu tại vị trí row, col theo từng hướng Sử dụng đệ quy
-         */
-        static void checkQ(int row, int col, int direct) {
-            int r = row + Q_row[direct];
-            int c = col + Q_col[direct];
-
-            if (r <= QueensKnightsPawns.row && r >= 0 && c <= QueensKnightsPawns.col && c >= 0) {
-                // Ô nào đang an toàn thì sẽ bị ăn
-                // Cho thành Unsafe
-                if (board[r][c] == Safe) {
-                    board[r][c] = Unsafe;
-                    sum--;
-                    checkQ(r, c, direct);
-                } else if (board[r][c] == Unsafe) {
-                    // Trường hợp này là đã tính trước đó.
-                    checkQ(r, c, direct);
+            while(count_chess <= PAWN) {
+                count_pos = scanner.nextInt();
+                while(count_pos > 0) {
+                    x = scanner.nextInt();
+                    y = scanner.nextInt();
+                    array[x-1][y-1] = count_chess;
+                    count_pos--;
                 }
+                count_chess++;
             }
-        }
 
-        public static void main(String[] args) throws Exception{
-            System.setIn(new FileInputStream("C:\\Users\\hung\\IdeaProjects\\learn SRV\\src\\input_queensKnightsPawns.txt"));
-            Scanner scanner = new Scanner(System.in);
-            int tc = 0;
-            while (true) {
-                row = scanner.nextInt();
-                col = scanner.nextInt();
-                if (row == 0 && col == 0)
-                    break;
-                tc++;
-                sum = row * col;
-                row--;
-                col--;
-
-                // Ban đầu gán các ô là ô an toàn
-                board = new int[row + 1][col + 1];
-                for (int r = 0; r <= row; r++)
-                    for (int c = 0; c <= col; c++)
-                        board[r][c] = Safe;
-
-                int[][] rc = new int[2][100]; // Lưu thông tin con hậu, con mã
-                int[] num = new int[3]; // Số lượng con hậu, con mã, con tốt
-
-                // Tiếp tục nhập đầu vào
-                for (int i = 0; i < 3; i++) {
-                    num[i] = scanner.nextInt();
-                    for (int j = 0; j < num[i]; j++) {
-                        int r = scanner.nextInt();
-                        int c = scanner.nextInt();
-                        board[r - 1][c - 1] = i;
-                        if (i != 2) {
-                            rc[i][j] = r - 1;
-                            rc[i][j] = c - 1;
+            for(i=0; i<m; i++) {
+                for(j=0; j<n; j++) {
+                    if(array[i][j] == KNIGHT) {
+                        for(k=0; k<8; k++) {
+                            int tempX = i + knight_moveX[k];
+                            int tempY = j + knight_moveY[k];
+                            if(tempX<0 || tempX>=m || tempY<0 || tempY>=n || array[tempX][tempY] != SAFE)
+                                continue;
+                            array[tempX][tempY] = NOT_SAFE;
+                        }
+                    } else if(array[i][j] == QUEEN) {
+                        for(k=0; k<2; k++) {
+                            int tempX = i + Q_straight_move[k];
+                            int tempY = j + Q_straight_move[k];
+                            while(true) {
+                                if(tempX<0 || tempX>=m || (array[tempX][j]!=SAFE && array[tempX][j]!=NOT_SAFE))
+                                    break;
+                                array[tempX][j] = NOT_SAFE;
+                                tempX += Q_straight_move[k];
+                            }
+                            while(true) {
+                                if(tempY<0 || tempY>=n || (array[i][tempY]!=SAFE && array[i][tempY]!=NOT_SAFE))
+                                    break;
+                                array[i][tempY] = NOT_SAFE;
+                                tempY += Q_straight_move[k];
+                            }
+                        }
+                        for(k=0; k<4; k++) {
+                            int tempX = i+Q_cross_moveX[k];
+                            int tempY = j+Q_cross_moveY[k];
+                            do {
+                                if(tempX<0 || tempX>=m || tempY<0 || tempY>=n || (array[tempX][tempY]!=SAFE && array[tempX][tempY]!=NOT_SAFE))
+                                    break;
+                                array[tempX][tempY] = NOT_SAFE;
+                                tempX += Q_cross_moveX[k];
+                                tempY += Q_cross_moveY[k];
+                            } while (true);
                         }
                     }
                 }
-
-                // Tính tổng các ô rảnh mà chưa có con nào đang đứng
-                sum -= num[Q] + num[K] + num[P];
-
-                // Duyệt các con mã
-                for (int j = 0; j < num[K]; j++)
-                    sum -= checkK(rc[K][j], rc[K][j]);
-
-                // Duyệt các con hậu
-                for (int j = 0; j < num[Q]; j++)
-                    for (int i = 0; i < 8; i++)
-                        checkQ(rc[Q][j], rc[Q][j], i);
-
-                System.out.println("Board " + tc + " has " + sum + " safe squares.");
             }
-            scanner.close();
+
+            for(i=0; i<m; i++) {
+                for(j=0; j<n; j++) {
+                    if(array[i][j] == SAFE) {
+                        count_safe++;
+                    }
+                }
+            }
+
+            System.out.println("Board " + round++ + " has " + count_safe + " safe squares.");
         }
+    }
 }
